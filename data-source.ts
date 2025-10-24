a1f4bd7c9e63dacfd4f154d6ab18ba43bbad9fbd
JustQu4n <nguyenanhquan01102002@gmail.com>
2025-10-24 17:36:45 +0700

    fix datasource

--- data-source.ts
+++ data-source.ts
@@ -1,22 +1,19 @@
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 
 // Load environment variables
 config();
 
-const configService = new ConfigService();
-
 export default new DataSource({
   type: 'postgres',
-  host: configService.get('DB_HOST', 'localhost'),
-  port: configService.get('DB_PORT', 5432),
-  username: configService.get('DB_USERNAME', 'careervibe'),
-  password: configService.get('DB_PASSWORD', 'Careervibe@123'),
-  database: configService.get('DB_DATABASE', 'careervibe_db'),
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['src/database/entities/**/*.entity{.ts,.js}'],
   migrations: ['src/database/migrations/*{.ts,.js}'],
   migrationsTableName: 'migrations',
   synchronize: false,
-  logging: configService.get('DB_LOGGING', 'false') === 'true',
+  logging: process.env.DB_LOGGING === 'true',
 });

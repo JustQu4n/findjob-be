1f116e416cf3335aec6fff4fdea612c0fc649500
JustQu4n <nguyenanhquan01102002@gmail.com>
2025-11-09 23:17:26 +0700

    feat(application-manager): implement application management for employers with status update functionality

--- src/modules/employer/employer.module.ts
+++ src/modules/employer/employer.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JobPostModule } from './job-post/job-post.module';
 import { ProfileModule } from './profile/profile.module';
+import { ApplicationManagerModule } from './application-manager/application-manager.module';
 
 @Module({
-  imports: [JobPostModule, ProfileModule],
+  imports: [JobPostModule, ProfileModule, ApplicationManagerModule],
   controllers: [],
   providers: [],
 })

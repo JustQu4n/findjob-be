a1fe2d8abbb5e3a8b88a6671bd2f11019b393ce9
JustQu4n <nguyenanhquan01102002@gmail.com>
2025-11-19 10:37:29 +0700

    refactor: remove outdated migration files and add initial schema and seed skills migration

--- src/database/migrations/1763372717081-InitialSchema.ts
+++ src/database/migrations/1763372717081-InitialSchema.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddProjectTable1763351499286 implements MigrationInterface {
-    name = 'AddProjectTable1763351499286'
+export class InitialSchema1763372717081 implements MigrationInterface {
+    name = 'InitialSchema1763372717081'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "role_permissions" DROP CONSTRAINT "FK_17022daf3f885f7d35423e9971e"`);
@@ -12,12 +12,10 @@ export class AddProjectTable1763351499286 implements MigrationInterface {
         await queryRunner.query(`DROP INDEX "public"."IDX_b23c65e50a758245a33ee35fda"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_178199805b901ccd220ab7740e"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_17022daf3f885f7d35423e9971"`);
-        await queryRunner.query(`CREATE TABLE "projects" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "job_seeker_id" uuid NOT NULL, "title" text NOT NULL, "description" text, "role" text, "start_date" date NOT NULL, "end_date" date, "is_current" boolean NOT NULL DEFAULT false, "project_url" text, "repo_url" text, "company_name" text, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_6271df0a7aed1d6c0691ce6ac50" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_178199805b901ccd220ab7740e" ON "role_permissions" ("role_id") `);
         await queryRunner.query(`CREATE INDEX "IDX_17022daf3f885f7d35423e9971" ON "role_permissions" ("permission_id") `);
         await queryRunner.query(`CREATE INDEX "IDX_87b8888186ca9769c960e92687" ON "user_roles" ("user_id") `);
         await queryRunner.query(`CREATE INDEX "IDX_b23c65e50a758245a33ee35fda" ON "user_roles" ("role_id") `);
-        await queryRunner.query(`ALTER TABLE "projects" ADD CONSTRAINT "FK_4307fa8428faff7e16f72b10ac8" FOREIGN KEY ("job_seeker_id") REFERENCES "job_seekers"("job_seeker_id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "user_roles" ADD CONSTRAINT "FK_87b8888186ca9769c960e926870" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "user_roles" ADD CONSTRAINT "FK_b23c65e50a758245a33ee35fda1" FOREIGN KEY ("role_id") REFERENCES "roles"("role_id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "role_permissions" ADD CONSTRAINT "FK_178199805b901ccd220ab7740ec" FOREIGN KEY ("role_id") REFERENCES "roles"("role_id") ON DELETE CASCADE ON UPDATE NO ACTION`);
@@ -29,12 +27,10 @@ export class AddProjectTable1763351499286 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "role_permissions" DROP CONSTRAINT "FK_178199805b901ccd220ab7740ec"`);
         await queryRunner.query(`ALTER TABLE "user_roles" DROP CONSTRAINT "FK_b23c65e50a758245a33ee35fda1"`);
         await queryRunner.query(`ALTER TABLE "user_roles" DROP CONSTRAINT "FK_87b8888186ca9769c960e926870"`);
-        await queryRunner.query(`ALTER TABLE "projects" DROP CONSTRAINT "FK_4307fa8428faff7e16f72b10ac8"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_b23c65e50a758245a33ee35fda"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_87b8888186ca9769c960e92687"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_17022daf3f885f7d35423e9971"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_178199805b901ccd220ab7740e"`);
-        await queryRunner.query(`DROP TABLE "projects"`);
         await queryRunner.query(`CREATE INDEX "IDX_17022daf3f885f7d35423e9971" ON "role_permissions" ("permission_id") `);
         await queryRunner.query(`CREATE INDEX "IDX_178199805b901ccd220ab7740e" ON "role_permissions" ("role_id") `);
         await queryRunner.query(`CREATE INDEX "IDX_b23c65e50a758245a33ee35fda" ON "user_roles" ("role_id") `);
